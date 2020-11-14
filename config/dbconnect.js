6d02afefcda2cf0042e7c7b8bf4eaeb25faf4bc0
Bing Z <bing@bings-mbp.lan>
2020-11-13 23:06:24 -0700

    added sucess n error logs on db connection

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-module.exports = () => { 
-
-    mongoose.connect("mongodb://localhost/workout", {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-});
-
-}
+module.exports = () => {
+	mongoose
+		.connect("mongodb://localhost/workout", {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			console.log("mongodb cononected...");
+		})
+		.catch((error) => {
+			console.log(error.message);
+		});
+};
