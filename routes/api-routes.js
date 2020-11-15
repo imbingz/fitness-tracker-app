614fa2bdef7d5673dcc4ef1afc6051da2f730911
Bing Z <bing@bings-mbp.lan>
2020-11-15 15:08:09 -0700

    added post n put route handlers

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,15 +12,31 @@ router.get("/", async (req, res) => {
 		const result = await db.Workout.find({});
 		res.json(result);
 	} catch (error) {
-		console.log(error.message);
+		res.status(500).send(error);
 	}
 });
 
 //Route for POST "api/workouts/"
 // router.post("/", createWorkout);
+router.post("/", async ({ body }, res) => {
+	try {
+		const result = await db.Workout.create(body);
+		res.json(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
 //Route for PUT "/api/workouts/workout_id"
 // router.put("/:id", addExercise);
+router.put("/:id", async (req, res) => {
+	try {
+		const result = await db.Workout.findByIdAndUpdate(req.params.id, req.body, { new: true });
+		res.json(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
 
 //Route for GET "/api/workouts/range"
 // router.get("/range", getWorkoutInRange);
