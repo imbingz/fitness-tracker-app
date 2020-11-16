d9910ed411f3c2e964c6c16476af770ba394d01b
Bing Z <bing@bings-mbp.lan>
2020-11-15 19:14:20 -0700

    added totalDuration

--- models/Workout.js
+++ models/Workout.js
@@ -4,47 +4,62 @@ const { Schema } = mongoose;
 
 //Setup Workout schema
 
-const WorkoutSchema = new Schema({
-	day: {
-		type: Date,
-		required: true,
-		default: Date.now,
-	},
-	totalDuration: Number,
-	exercises: [
-		{
-			type: {
-				type: String,
-				required: true,
-			},
-			name: {
-				type: String,
-				trim: true,
-				required: true,
-				lowercase: true,
-			},
-			duration: {
-				type: Number,
-				trim: true,
-			},
-			weight: {
-				type: Number,
-				trim: true,
-			},
-			reps: {
-				type: Number,
-				trim: true,
-			},
-			sets: {
-				type: Number,
-				trim: true,
-			},
-			distance: {
-				type: Number,
-				trim: true,
-			},
+const WorkoutSchema = new Schema(
+	{
+		day: {
+			type: Date,
+			required: true,
+			default: Date.now,
 		},
-	],
+		exercises: [
+			{
+				type: {
+					type: String,
+					required: true,
+				},
+				name: {
+					type: String,
+					trim: true,
+					required: true,
+					lowercase: true,
+				},
+				duration: {
+					type: Number,
+					trim: true,
+				},
+				weight: {
+					type: Number,
+					trim: true,
+				},
+				reps: {
+					type: Number,
+					trim: true,
+				},
+				sets: {
+					type: Number,
+					trim: true,
+				},
+				distance: {
+					type: Number,
+					trim: true,
+				},
+			},
+		],
+	},
+	{
+		toObject: {
+			virtuals: true,
+		},
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+	return this.exercises.reduce((total, exercise) => {
+		return total + exercise.duration;
+	}, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
