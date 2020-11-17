4291e8d762eeae5608149d573441f041c75f5946
Bing Z <bing@bings-mbp.lan>
2020-11-17 09:58:17 -0700

    resetp server and d connection in server.js

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const dotenv = require("dotenv").config();
 //Import packages
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 //Import api-routes
 const apiRoute = require("./routes/api-routes");
 const htmlRoute = require("./routes/html-routes");
-
-//Connect to mongodb
-require("./config/dbconnect")();
 
 //Setup PORT
 const PORT = process.env.PORT || 3000;
@@ -28,7 +26,20 @@ app.use(express.static("public"));
 app.use("/api/workouts", apiRoute);
 app.use("/", htmlRoute);
 
-//Setup Server
-app.listen(PORT, () => {
-	console.log(`listening on PORT ${PORT}, http://localhost:${PORT}`);
-});
+//Connect to mongodb atlas and server
+mongoose
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		//Setup Server after connecting to db
+		app.listen(PORT, () => {
+			console.log(`listening on PORT ${PORT}, http://localhost:${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error.message);
+	});
